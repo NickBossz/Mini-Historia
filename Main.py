3003a0f1496ebf2392656e1e6f09db83f8824858
Nicolas Mateus de Castro Silva <nicolasmateusdecastrosilva@gmail.com>
2023-05-29 13:13:31 -0300

    mini historia

--- Main.py
+++ Main.py
@@ -2,7 +2,6 @@ from time import sleep
 import Banco
 import LojaCarros
 import Padaria
-import LugarMisterioso
 import PersonagemPrincipal
 import Casa
 
@@ -15,14 +14,12 @@ sleep(1.2)
 print(f"Bem vindo {Nome}! Seu saldo inicial no banco será de R${PersonagemPrincipal.ContaPrincipal.saldo_atual} e em dinheiro será {PersonagemPrincipal.ContaPrincipal.Dinheiro}! Aproveite e faça um bom uso!")
 sleep(1.5)
 while (True):
-    print("Onde você gostaria de ir?\nLoja de carros, padaria, lugar misterioso, sua casa ou banco? (l/p/m/c/b)")
+    print("Onde você gostaria de ir?\nLoja de carros, padaria, sua casa ou banco? (l/p/m/c/b)")
     Resposta = input()
     if Resposta == "l":
         LojaCarros.LojaDeCarros()
     if Resposta == "p":
         Padaria.Padaria()
-    if Resposta == "m":
-        LugarMisterioso.LugarMisterioso()
     if Resposta == "b":
         Banco.Banco()
     if Resposta == "c":
